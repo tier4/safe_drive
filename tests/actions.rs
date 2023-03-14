890654415cd155cf973fd64a1251d1d594fb918d
Seio Inoue <seio.inoue@tier4.jp>
2023-03-14 16:47:56 +0900

    Use single thread for action tests

--- tests/actions.rs
+++ tests/actions.rs
@@ -15,32 +15,20 @@ use std::{thread, time::Duration};
 fn test_action() -> Result<(), DynError> {
     let ctx = Context::new()?;
 
-    let node_client = ctx.create_node("test_action_client_node", None, Default::default())?;
-
-    let mut client: Client<MyAction> = Client::new(node_client, "test_action", None)?;
-
     // start the action server
-    let handle = thread::spawn(move || {
-        let node_server = ctx
-            .create_node("test_action_server_node", None, Default::default())
-            .unwrap();
-        let mut server: Server<MyAction> = Server::new(node_server, "test_action", None, |req| {
-            println!("Goal request received: {:?}", req);
-            true
-        })
+    let node_server = ctx
+        .create_node("test_action_server_node", None, Default::default())
         .unwrap();
 
-        println!("server: waiting for goal requests...");
-        match server.try_recv_goal_request() {
-            RecvResult::Ok(_) => println!("server: accepted goal"),
-            RecvResult::RetryLater(_) => {
-                // println!("server: waiting for goal requests...");
-            }
-            RecvResult::Err(err) => println!("server: error: {:?}", err),
-        }
-    });
+    let mut server: Server<MyAction> = Server::new(node_server, "test_action", None, |req| {
+        println!("Goal request received: {:?}", req);
+        true
+    })
+    .unwrap();
 
-    thread::sleep(Duration::from_millis(100));
+    // start the action client
+    let node_client = ctx.create_node("test_action_client_node", None, Default::default())?;
+    let mut client: Client<MyAction> = Client::new(node_client, "test_action", None)?;
 
     // wait for action server
     // loop {
@@ -59,6 +47,15 @@ fn test_action() -> Result<(), DynError> {
     // TODO: ergonomic apis
     // TODO: generate UUID w/ uuid crate. rclcpp's ClientBase::generate_goal_id
     // does not conform to the UUID v4 standard, strictly speaking.
+    // client.send_goal_with_uuid(goal, goal_id, |resp| {
+    //         println!(
+    //             "Goal response received: accepted = {}, timestamp = {:?}",
+    //             resp.accepted, resp.stamp
+    //         );
+    //     }
+    // )?;
+    thread::sleep(Duration::from_millis(100));
+
     client.send_goal_request(
         &goal_request,
         Box::new(|resp| {
@@ -69,19 +66,20 @@ fn test_action() -> Result<(), DynError> {
         }),
     )?;
 
-    // TODO: UUID can't be cloned?
-    let mut goal_id = UUID::new().unwrap();
-    goal_id.uuid = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    let result_req = MyAction_GetResult_Request { goal_id };
-    client.send_result_request(
-        &result_req,
-        Box::new(|resp| {
-            println!(
-                "Result response received: status = {}, result = {}",
-                resp.status, resp.result.b
-            );
-        }),
-    )?;
+    // wait for goal request, then send response
+    loop {
+        match server.try_recv_goal_request() {
+            RecvResult::Ok(_) => {
+                println!("server: accepted goal");
+                break;
+            }
+            RecvResult::RetryLater(_) => {
+                println!("server: waiting for goal requests...");
+            }
+            RecvResult::Err(e) => println!("{:?}", e),
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
 
     // receive goal response
     loop {
@@ -114,6 +112,20 @@ fn test_action() -> Result<(), DynError> {
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 
+    // TODO: UUID can't be cloned?
+    let mut goal_id = UUID::new().unwrap();
+    goal_id.uuid = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    let result_req = MyAction_GetResult_Request { goal_id };
+    client.send_result_request(
+        &result_req,
+        Box::new(|resp| {
+            println!(
+                "Result response received: status = {}, result = {}",
+                resp.status, resp.result.b
+            );
+        }),
+    )?;
+
     // get result
     loop {
         match client.try_recv_result_response() {
@@ -125,8 +137,6 @@ fn test_action() -> Result<(), DynError> {
         }
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
-
-    handle.join().unwrap();
 
     Ok(())
 }
